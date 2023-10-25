134fc4126c125575e1ec9bd971a8c3ea3108d9b5
Trouthe <anthony.hajjar00@gmail.com>
2023-10-25 18:11:32 +0300

    Design Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavigationComponent,
-    CatalogComponent
+    CatalogComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
